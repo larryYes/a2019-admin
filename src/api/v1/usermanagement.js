371a277312d87be98841b454864b83cf302543de
Aritimes <cd.aritimes@gmail.com>
2021-03-23 15:51:11 +0800

    添加：角色列表页面

--- src/api/v1/usermanagement.js
+++ src/api/v1/usermanagement.js
@@ -1,12 +1,20 @@
 import request from '@/utils/request'
 
 export default {
+    // 获取所有用户
     getAllUsers() {
         return request({
             url: '/url/v1/usermanagement/users/list',
             method: 'post',
             data: {}
         })
+    },
+    // 获取所有角色
+    getAllRole(){
+        return request({
+            url: `/url/v1/usermanagement/roles/list`,
+            method: `post`,
+            data: {}
+        })
     }
 }
-
---
+++ src/views/v1/role/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <el-table :data="roleList" style="width: 100%">
+    <el-table-column prop="id" label="id" width="100"></el-table-column>
+    <el-table-column prop="name" label="姓名" width="180"></el-table-column>
+    <el-table-column prop="description" label="描述" width="300"></el-table-column>
+    <el-table-column prop="countPrincipals" label="countPrincipals" width="300"></el-table-column>
+    <el-table-column prop="version" label="版本" width="300"></el-table-column>
+    <el-table-column prop="updatedOn" label="更新时间" width="300"></el-table-column>
+  </el-table>
+</template>
+
+<script>
+
+import usermanagement from "@/api/v1/usermanagement.js";
+
+export default {
+  data() {
+    return {
+      roleList: [],
+    };
+  },
+
+  created(){
+      this.getAllRole()
+  },
+  methods: {
+      //返回角色信息的列表
+      getAllRole(){
+          usermanagement.getAllRole().then(response =>{
+        this.roleList = response.list
+      })
+      .catch((error) =>{
+        console.log(error);
+      })
+      }
+  },
+};
+</script>
