3233f5b9b0556277e51dcd67ceb2a328a44fdea1
Aritimes <cd.aritimes@gmail.com>
2021-03-25 16:17:56 +0800

    修改： 优化侧边栏

--- src/views/v2/Bot Execution Orchestrator/inprogress.vue
+++ src/views/v2/Bot Execution Orchestrator/inprogress.vue
@@ -1,33 +1,26 @@
 <template>
   <el-table :data="inprogressList" style="width: 100%">
-    <el-table-column prop="automationName" label="名称" width="180" ></el-table-column>
+    <el-table-column prop="jobExecutionStatus" label="状态" width="180"></el-table-column>
+    <el-table-column prop="automationName" label="名称" width="180"></el-table-column>
     <el-table-column prop="startDateTime" label="起始时间" width="180"></el-table-column>
     <el-table-column prop="command" label="描述" width="100"></el-table-column>
-    <el-table-column
-      label="操作"
-      width="120">
+    <el-table-column label="操作" width="120">
       <template slot-scope="scope">
-        <el-button
-          @click.prevent="skipDetailPage(scope.row.id)"
-          type="text"
-          size="small">
-          项目详情
-        </el-button>
+        <el-button @click.prevent="skipDetailPage(scope.row.id)" type="text" size="small">项目详情</el-button>
       </template>
-      
     </el-table-column>
-    
   </el-table>
 </template>
 
 <script>
 import botExcecutionOrchestrator from "@/api/v2/botExecutionOrchestrator.js";
-import common from '@/api/v1/common'
+import common from "@/api/v1/common";
 
 export default {
   data() {
     return {
       inprogressList: [],
+      number: 0
     };
   },
   created() {
@@ -44,12 +37,23 @@ export default {
           console.log(error);
         });
     },
-    skipDetailPage(id){
-        this.$router.push({
-            path: `/activity/inProgress/${id}/detail`,
-            query: {param: this.param}
-        })
-    }
+    skipDetailPage(id) {
+      this.$router.push({
+        path: `/activity/inProgress/${id}/detail`,
+        query: { param: this.param },
+      });
+    },
   },
+  mounted(){
+    //10秒刷新一次数据
+    setInterval(()=> {
+      this.number = this.number + 1
+    }, 10000)
+  },
+  watch:{
+    number(){
+      // this.getAllInprogressBot()
+    }
+  }
 };
 </script>
